d36ad963e2d839bfe04e5efe5b37cb29e99723c2
Maria Hildebrandt <hildebrandt.maria@googlemail.com>
2018-03-19 23:37:43 +0100

    Neuer brunch Library

--- src/main/java/com/hildebrandt/uni/repositories/LectureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hildebrandt.uni.repositories;
-
-import com.hildebrandt.uni.domain.Lecture;
-import org.springframework.data.repository.CrudRepository;
-
-public interface LectureRepository extends CrudRepository<Lecture, Long> {
-}
--- src/main/java/com/hildebrandt/uni/repositories/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hildebrandt.uni.repositories;
-
-import com.hildebrandt.uni.domain.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, Long>{
-}
